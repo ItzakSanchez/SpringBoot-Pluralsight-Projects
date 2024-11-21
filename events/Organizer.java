db4f97966fd9da388eb74ebd19d80af93de8d68b
Edgar Itzak Sanchez Rogers <109997241+ItzakSanchez@users.noreply.github.com>
2024-11-21 17:37:33 -0600

    Delete events directory

--- events/Organizer.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.springboot.tickets.events;
-
-public record Organizer(
-        int id,
-        String name,
-        String description) {
-}
--- events/Venue.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight.springboot.tickets.events;
-
-public record Venue(
-        int id,
-        String name,
-        String street,
-        String city,
-        String country) {
-}
