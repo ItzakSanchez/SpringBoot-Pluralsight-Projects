94786b420b9dbab022e3b7157029bba49aecb6e4
Edgar Itzak Sanchez Rogers <109997241+ItzakSanchez@users.noreply.github.com>
2024-11-26 09:00:26 -0600

    Add files via upload

---
+++ tickets2/event-service/src/main/java/eventservice/dev/edgaritzak/VenueRepository.java
@@ -0,0 +1,9 @@
+package eventservice.dev.edgaritzak;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VenueRepository extends JpaRepository<Venue, Integer>{
+
+}
