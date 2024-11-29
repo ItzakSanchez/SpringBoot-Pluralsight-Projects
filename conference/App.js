e5dd2084fd063a148751417483a6b1da7e843900
Edgar Itzak Sanchez Rogers <109997241+ItzakSanchez@users.noreply.github.com>
2024-11-29 12:42:18 -0600

    Add files via upload

--- conference/App.js
+++ conference/App.js
@@ -1,40 +1,44 @@
 import React from "react";
 import './App.css';
-
-class App extends React.Component{
-  constructor(props){
-      super(props)
-      this.state={
-        items:[],
-        DataIsLoaded: false
-      };
-  }
-
-  componentDidMount(){
-    fetch("http://localhost:8080/api/v1/user/getUser")
-      .then((resp)=> resp.json())
-      .then((json) => {
-        this.state({
-          items: json,
-          DataIsLoaded: true
-        });
-      })
-  }
-
-  render(){
-    const {DataIsLoaded, items} = this.state;
-    if(!DataIsLoaded) return <div>
-      <h1>Please wait...</h1></div>;
-
-    return(
-      <div className="App">
-        <h1>Fetch data from an api in react</h1>
-        <ol key={items.id}>
-          FullName: {items.name}
-        </ol>
-      </div>
+class App extends React.Component {
+  
+    // Constructor
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+  
+    // ComponentDidMount is used to
+    // execute the code
+    componentDidMount() {
+        fetch(
+"http://localhost:8080/api/v1/user/hello")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Please wait ... </h1> </div> ;
+  
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1>  
+                <ol key = { items.id } >
+                    Full Name: { items.name }
+                    </ol>
+        </div>
     );
-  }
 }
-
+}
+  
 export default App;
